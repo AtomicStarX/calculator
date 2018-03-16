7badbfe695e5ccea52af8758c1faecf0d1b30c2c
alu53788313f <alu53788313f@ti2-06>
2018-03-16 15:42:52 +0100

    Fixe equal action and number after coma separator

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -69,9 +69,10 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void displayResult() {
         String s = ""+accumulator;
+        System.out.println(s);
         if(s.contains(".")){
             s = s.replaceAll("0+$", "");
-            s = s.replaceAll(".$","");
+            s = s.replaceAll("\\.$","");
         }
         textFieldDisplay.setText(s);
         // BigDecimal number = new BigDecimal(accumulator);
@@ -431,9 +432,10 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void btnEqualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEqualActionPerformed
         if (!erase) {
-            erase = true;
+            //erase = true;
             calculateResult();
             displayResult();
+            operator = OperatorType.NONE;
         }
     }//GEN-LAST:event_btnEqualActionPerformed
 
